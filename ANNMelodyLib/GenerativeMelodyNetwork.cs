5527f019149f4ff040bcacc844df67c52a56d305
ReinaStreufert <49079389+ReinaStreufert@users.noreply.github.com>
2025-02-18 20:28:35 -0500

    ily hayley

--- ANNMelodyLib/GenerativeMelodyNetwork.cs
+++ ANNMelodyLib/GenerativeMelodyNetwork.cs
@@ -2,10 +2,12 @@
 
 namespace ANNMelodyLib
 {
-    // this will be a 2-layer neural network. the network has 9 inputs for each 32nd-note in the context window.
+    // this will be a 1-layer neural network. the network has 9 inputs for each 32nd-note in the context window.
     // each position in the context window must have either all 9 inputs at zero, or a singular one set to 1 with
     // with all the others being zero. this represents either silence, a specific scale note, or the 9th input=a continuation
-    // of the last playing note. backpropogation for learning, RMSProp for convergence, and a logistic activation function
+    // of the last playing note. the network has 9 outputs, each one is a probability value predicting the likelihood of the
+    // next position in the context window being the corresponding note. backpropogation for learning, RMSProp for convergence, 
+    // and a logistic activation function
 
     public struct GenerativeMelodyNetwork
     {
@@ -13,12 +15,38 @@ namespace ANNMelodyLib
         public float RMSPropBaseForgetRate { get; }
         public ArrayView<float> WeightVecs { get; }
         public ArrayView<float> SparsityVecs { get; }
+        public int ContextWindowLen { get; }
+        public int ParameterCount => ContextWindowLen * 9;
+        public int OutputCount => 9;
+
+        public RMSParameter GetParameter(int index)
+        {
+            var vecIdx = Grid.IdxX * ParameterCount + index;
+            return new RMSParameter(ref WeightVecs[vecIdx], ref SparsityVecs[vecIdx]);
+        }
+
+        public float FindOutputVec(ArrayView<float> inputVecs)
+        {
+            var weightedSum = 0f;
+            for (int i = 0; i < ParameterCount; i++)
+            {
+                var parameter = GetParameter(i);
+                weightedSum += inputVecs[i] * parameter.WeightVec;
+            }
+            return ANN.ActivateLogistic(weightedSum);
+        }
     }
 
     public ref struct RMSParameter
     {
         public ref float WeightVec;
         public ref float SlidingSparsityVec;
+
+        public RMSParameter(ref float weightVec, ref float slidingSparsityVec)
+        {
+            WeightVec = weightVec;
+            SlidingSparsityVec = slidingSparsityVec;
+        }
 
         public void AdjustWeight(float outputError, float baseLearnRate, float adaptRate)
         {
---
+++ ANNMelodyLib/TrainingContext.cs
@@ -0,0 +1,21 @@
+﻿using ILGPU;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANNMelodyLib
+{
+    public struct TrainingContext
+    {
+        public GenerativeMelodyNetwork Network;
+        public ArrayView<float> InputVecs;
+        public ArrayView<float> SampleOutputVecs;
+
+        public void AdjustNodeWeights()
+        {
+            var paramsPerNode = Network.ParametersPerNode;
+        }
+    }
+}
